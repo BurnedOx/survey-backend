504a04f77e2c1a945a838c8291834e9884694ec4
mainak1 <mainak@trevious.com>
2020-03-20 00:46:44 +0530

    unknown commit

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Survey
+from .models import Survey, SurveyType
 
 # Register your models here.
 
 admin.site.register(Survey)
+admin.site.register(SurveyType)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+status_choices = (
+    ('Active', 'active'),
+    ('Archived', 'archived'),
+    ('Deleted', 'deleted'),
+)
+
 
 class Survey(models.Model):
     name = models.CharField(max_length=200)
@@ -10,3 +18,18 @@ class Survey(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class SurveyType(models.Model):
+    type = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='created_by')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='updated_by')
+    status = models.CharField(max_length=200, choices=status_choices, default='Active')
+    description = models.TextField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.type
+
+
+class Vote(models.Model):
+    type_id = models.ForeignKey(SurveyType, on_delete=models.CASCADE)
